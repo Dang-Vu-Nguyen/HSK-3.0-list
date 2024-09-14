ef7503b0e81fd3afdb77f690869333fd0d9bbdaa
Dang-Vu-Nguyen <151882659+Dang-Vu-Nguyen@users.noreply.github.com>
2024-09-14 15:22:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,28 @@ df = pd.read_excel(excel_file, usecols=columns_to_display)
 st.set_page_config(page_title='HSK Vocabulary App')
 
 # App Title and Description
-st.title('Toàn Bộ 11092 Từ Vựng HSK 3.0 - Từ HSK1 đến HSK9')
-st.subheader('Đã Hoàn thành Từ Vựng Đến Hết HSK 5')
-
-# Additional information for users
+st.title('Toàn Bộ 11092 Từ Vựng HSK 3.0')
+st.subheader('Đã hoàn thành từ HSK1 đến HSK5')
 st.markdown('''
-- Xem video có audio: [YouTube Channel](https://www.youtube.com/@luyentiengtrung2)  
-- Xem thẻ từ vựng: [vunotes.com/tieng-trung](https://vunotes.com/tieng-trung)
+Theo cấu trúc HSK 3.0 mới, sau 2021, mỗi cấp độ sẽ cần số từ vựng như sau:
+- HSK1: 500 từ
+- HSK2: 772 từ
+- HSK3: 973 từ
+- HSK4: 1000 từ
+- HSK5: 1071 từ
+- HSK6: 1140 từ
+- HSK7-9: 5636 từ
+Tổng cộng là 11092 từ.
+
+Tại đây, mình đã lập danh sách tất cả 11092 từ vựng. Mỗi từ vựng bao gồm Pinyin, Hán Việt, nghĩa tiếng Việt, câu ví dụ, và cả dạng Phồn Thể.
+
+Hy vọng danh sách này sẽ hữu ích cho các bạn! Chúc các bạn học thật tốt nhé!
+
+- Học bằng video và audio: [Kênh YouTube Luyện Tiếng Trung 2](https://www.youtube.com/@luyentiengtrung2)  
+- Học bằng thẻ từ vựng: [vunotes.com/tieng-trung](https://vunotes.com/tieng-trung)  
+- Học theo cấu trúc HSK 2.0 (cũ): [Kênh YouTube Luyện Tiếng Trung](https://www.youtube.com/@luyentiengtrung)
 ''')
+
 
 # Display the dataframe in Streamlit
 st.dataframe(df)
