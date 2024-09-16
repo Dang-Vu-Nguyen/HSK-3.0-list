4b01c3b5540e7111d49f745e9e570f65e95c0b7c
Dang-Vu-Nguyen <151882659+Dang-Vu-Nguyen@users.noreply.github.com>
2024-09-15 18:01:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -245,7 +245,19 @@ hsk4_section = st.empty()
 timer5_section = st.empty()
 hsk5_section = st.empty()
 
+st.markdown('''
 
+Tài liệu được tổng hợp và biên soạn bởi Luyện Tiếng Trung 2. Xin vui lòng không sử dụng với mục đích thương mại mà không có sự cho phép của chúng mình.
+
+Nếu bạn thấy nội dung hữu ích và muốn ủng hộ chúng mình, bạn có thể cân nhắc tặng chúng mình một cốc cà phê tại:
+- Techcombank
+- 290667040209
+- NGUYEN THI HONG KHANH
+
+Cám ơn bạn rất nhiều ạ!
+
+
+''')
 
 while True:
         
